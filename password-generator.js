8e8edd7a7355787a33a1108a6119c0de94b673d7
Kevin Peguero <kevinpeguero2@gmail.com>
2020-02-24 17:00:41 -0700

    made changes to HTML/CSS

--- password-generator.js
+++ password-generator.js
@@ -1,4 +1,117 @@
-var numbers=[ "0","1","2","3","4","5","6","7","8","9"];
-var uppercahar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercahar=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var specialcahar=["~","!","@","#","$","%","&","*"];
+
+
+// Array of special characters to be included in password
+var specialCharacters = [
+    "@",
+    "%",
+    "+",
+    "\\",
+    "/",
+    "'",
+    "!",
+    "#",
+    "$",
+    "^",
+    "?",
+    ":",
+    ",",
+    ")",
+    "(",
+    "}",
+    "{",
+    "]",
+    "[",
+    "~",
+    "-",
+    "_",
+    "."
+  ];
+  
+  // Array of numeric characters to be included in password
+  var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+
+//   prompt("who won the 2019 nba finals?")
+
+//   confirm("true or false toronto wn nba finals")
+
+//   alert("has to be toronto raptors")
+
+// Function Password Length
+function passwordLength() {
+    var charLength = parseInt(prompt("how many characters in the password"))
+    console.log(charLength)
+
+    if (charLength >=8 && charLength <=128) {
+       alert("password is good")
+    } else {
+        alert("password needs to be between 8 through 128")
+    }
+
+
+}
+passwordLength()
+
+
+
+
+
